b7fb63b5b836d737b203c6a8107c0c1b764df841
Kevin Chu <kevinchu1905@gmail.com>
2016-08-28 02:14:27 +0800

    WOOO WE WIN
    dfsadfdsfd

--- index.js
+++ index.js
@@ -2,33 +2,44 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var apiAI = require('apiai');
-var api = apiAI('e5e584257d164eab95ffd6038c9f56f7');
+var api = apiAI('f9acff6103814cf4a834f36232bb78d3');
+app.set('view engine', 'ejs');
+
+var chatRoom = 0;
 
 // Routes
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.render(__dirname + '/index.ejs', {
+		chatRoom: chatRoom
+	});
+	chatRoom = chatRoom + 1;
 });
 
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		io.emit('chat message', "YOU: " + msg);
 
-		var request = api.textRequest(msg);
+	socket.on('new', function(room){
+		socket.join(chatRoom);
+		console.log('join new chatroom.')
+	});
+
+	socket.on('send', function(data){
+		io.to(data.room).emit('message', data);
+
+		var request = api.textRequest(data.message);
 		request.on('response', function(response) {
-			result = response.result.fulfillment.speech;
-			io.emit('chat message', "BOT: " + result);
+			result = {message: response.result.fulfillment.speech};
+			io.to(data.room).emit('message', result);
 		});
 		request.on('error', function(error) {
 			console.log(error);
 		});
 		request.end();
 
+		console.log('sending message to' + data.room + ' || '+ data.message)
 	});
-	console.log('LOG: user connected');
-	socket.on('disconnect', function(){
-		console.log('LOG: user disconnected');
-	});
+
 });
+
 
 http.listen(3000, function(){
 	console.log('LOG: listening on port 3000');
